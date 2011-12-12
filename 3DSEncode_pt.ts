6476e809e6726a89b9a0889dfff8c4ceff540927
Claudio Roberto França Pereira <spideybr@gmail.com>
2011-12-12 14:16:05 -0200

    Added translation files. They aren't translated yet.

---
+++ 3DSEncode_pt.ts
@@ -0,0 +1,432 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>Q3DSEncode</name>
+    <message>
+        <location filename="q3dsencode.ui" line="17"/>
+        <source>3DSEncode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="38"/>
+        <source>Video File or URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="54"/>
+        <source>Open...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="73"/>
+        <source>2D/3D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="94"/>
+        <source>3D Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="127"/>
+        <source>Side-By-Side</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="137"/>
+        <source>Top-Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="144"/>
+        <source>Row
+Interleaved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="152"/>
+        <source>Column
+Interleaved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="176"/>
+        <source>Reversed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="202"/>
+        <source>A/V</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="214"/>
+        <source>Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="232"/>
+        <source>Channels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="258"/>
+        <source>1 - Mono</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="263"/>
+        <source>2 - Stereo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="283"/>
+        <source>Sample
+Rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="316"/>
+        <source>22.05KHz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="321"/>
+        <source>44.1KHz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="326"/>
+        <source>48KHz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="331"/>
+        <source>96KHz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="342"/>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="367"/>
+        <source>Compression</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="460"/>
+        <source>Best</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="473"/>
+        <source>Smaller</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="526"/>
+        <source>FPS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="555"/>
+        <source>10fps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="560"/>
+        <source>12fps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="565"/>
+        <source>15fps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="570"/>
+        <source>20fps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="575"/>
+        <source>24fps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="580"/>
+        <source>30fps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="594"/>
+        <source>Resolution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="617"/>
+        <source>400x240</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="622"/>
+        <source>480x240</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="638"/>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="671"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="678"/>
+        <source>yyyy-MM-dd hh:mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="688"/>
+        <source>Threads</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="725"/>
+        <source>Split in 10 minute videos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="778"/>
+        <source>Output Video(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="794"/>
+        <source>Select...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="815"/>
+        <location filename="q3dsencode.ui" line="881"/>
+        <source>&amp;Convert</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="832"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="842"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="849"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="859"/>
+        <source>&amp;Open...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="862"/>
+        <location filename="q3dsencode.cpp" line="134"/>
+        <source>Open source video file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="865"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="870"/>
+        <source>&amp;Save to...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="873"/>
+        <source>Set output video file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="876"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="884"/>
+        <source>Start convertion process</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="887"/>
+        <source>Ctrl+T</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="892"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="895"/>
+        <source>Close the application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="898"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="903"/>
+        <source>&amp;Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="906"/>
+        <source>Change the program language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="909"/>
+        <source>Ctrl+L</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="914"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="917"/>
+        <source>About 3DSEncoder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.ui" line="922"/>
+        <source>Load last used settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="65"/>
+        <source>Encoding video file...
+This may take several minutes to complete.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="118"/>
+        <source>3DSEncodeGUI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="118"/>
+        <source>&lt;h1&gt;3DSEncode&lt;/h1&gt;&lt;p&gt;3DSEncode is a all-in-one encoding solution to the 3DS, based on libav and youtube-dl.&lt;/p&gt;&lt;br&gt;&lt;p&gt;This is free software, made by Spidey (&lt;a href=&apos;mailto:spideybr@gmail.com&apos;&gt;spideybr@gmail.com&lt;/a&gt;).&lt;/p&gt;&lt;p&gt;This software is licensed under the GPLv3.&lt;/p&gt;&lt;br&gt;&lt;p&gt;Thanks to GBATemp users spinal_cord, Lokao0, SifJar and DiscostewSM&lt;/p&gt;&lt;p&gt;Application icon by CristopherOS (&lt;a href=&apos;http://cristopheros.deviantart.com/&apos;&gt;http://cristopheros.deviantart.com/&lt;/a&gt;).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="129"/>
+        <source>Not Available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="129"/>
+        <source>This feature is not yet available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="134"/>
+        <source>Video files(*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="140"/>
+        <source>Save converted video to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="140"/>
+        <source>AVI video file(*.avi)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="215"/>
+        <source>Encoding Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="215"/>
+        <source>An error occurred while encoding the video.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="217"/>
+        <source>Encoding Completed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="217"/>
+        <source>Conversion is completed.
+Now, transfer %1 to your 3DS SD card.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="234"/>
+        <source>Input Validation Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="239"/>
+        <source>Source video not informed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="243"/>
+        <source>Source video is neither a valid video file or an URL.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="249"/>
+        <source>[1-9][0-9]* ?(fps)?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="252"/>
+        <source>Invalid FPS value specified.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="260"/>
+        <source>Output video file wasn&apos;t informed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="269"/>
+        <source>Output video file should be in
+LLL_DD01.avi format (e.g., VID_1201.avi).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="275"/>
+        <source>Files may be overwritten</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="q3dsencode.cpp" line="275"/>
+        <source>There are some files in the output directory that may be overwritten during the encoding process.
+Are you sure you want to continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
